79acea203210aac5c06100323671de98e8abc426
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-14 14:02:31 -0300

    feat: AddGame model created

---
+++ source/model/Games/AddGame/index.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv');
+const { ObjectId } = require('mongodb');
+const dbConnection = require('../../connection');
+
+dotenv.config();
+
+const { GAMES_COLLECTION } = process.env;
+
+const AddGame = async (newGame) => {
+  const db = await dbConnection();
+
+  const { insertedId: id } = await db.collection(GAMES_COLLECTION).insertOne(newGame);
+
+  const gameAdded = {
+    ...newGame,
+    _id: ObjectId(id),
+  };
+
+  return gameAdded;
+};
+
+module.exports = AddGame;
