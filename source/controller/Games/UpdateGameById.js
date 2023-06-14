d99aa119a26af79351a1c05600d99b1191891acd
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-14 18:05:49 -0300

    feat: UpdateGameById controller created

---
+++ source/controller/Games/UpdateGameById.js
@@ -0,0 +1,22 @@
+const { StatusCodes } = require('http-status-codes');
+
+const ErrorCreator = require('../../helpers/ErrorCreator');
+const gamesService = require('../../service/Games');
+
+const UpdateGameById = async (req, res, next) => {
+  let updatedGame;
+  try {
+    const { id } = req.params;
+    updatedGame = await gamesService.UpdateGameById(id, req.body);
+
+    if (updatedGame instanceof ErrorCreator) {
+      return res.status(updatedGame.status).json({ message: updatedGame.customMessage });
+    }
+  } catch (e) {
+    next(e);
+  }
+
+  return res.status(StatusCodes.OK).json({ updatedGame });
+};
+
+module.exports = UpdateGameById;
