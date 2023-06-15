17e26562c8908c5f8211d20fb46dbb061895c588
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-15 10:04:20 -0300

    feat: GetGameById controller created

--- source/controller/Games/gamesRouter.js
+++ source/controller/Games/gamesRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const AddGame = require('./AddGame');
 const UpdateGameById = require('./UpdateGameById');
+const GetGameById = require('./GetGameById');
 
 const gamesRouter = express.Router({ mergeParams: true });
 
+gamesRouter.get('/games/:id', GetGameById);
 gamesRouter.post('/games', AddGame);
 gamesRouter.put('/games/:id', UpdateGameById);
 
