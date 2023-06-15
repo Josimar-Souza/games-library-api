65f0e88f104322deb9281dea9039b52d02634d49
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-15 09:48:59 -0300

    feat: GetGameById service created

--- source/service/Games/index.js
+++ source/service/Games/index.js
@@ -1,9 +1,11 @@
 const AddGame = require('./AddGame');
 const UpdateGameById = require('./UpdateGameById');
+const GetGameById = require('./GetGameById');
 
 const gamesService = {
   AddGame,
   UpdateGameById,
+  GetGameById,
 };
 
 module.exports = gamesService;
