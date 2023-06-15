e2b2ceb088636f511fbfc86a4ea4a22861cbab45
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-15 11:49:38 -0300

    feat: DeleteGameById service created

--- source/service/Games/index.js
+++ source/service/Games/index.js
@@ -2,12 +2,14 @@ const AddGame = require('./AddGame');
 const UpdateGameById = require('./UpdateGameById');
 const GetGameById = require('./GetGameById');
 const GetAllGames = require('./GetAllGames');
+const DeleteGameById = require('./DeleteGameById');
 
 const gamesService = {
   AddGame,
   UpdateGameById,
   GetGameById,
   GetAllGames,
+  DeleteGameById,
 };
 
 module.exports = gamesService;
