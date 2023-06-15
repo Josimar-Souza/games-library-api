754d45abb878f2f71cf49e9e85a69c5c18d2cdfb
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-15 13:17:26 -0300

    feat: GetAllCategories service created

--- source/service/Categories/index.js
+++ source/service/Categories/index.js
@@ -1,7 +1,9 @@
 const AddCategory = require('./AddCategory');
+const GetAllCategories = require('./GetAllCategories');
 
 const categoriesService = {
   AddCategory,
+  GetAllCategories,
 };
 
 module.exports = categoriesService;
