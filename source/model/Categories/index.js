d8c4a8e3d06f132df5ad4d9248d850c22866ff5b
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-15 13:49:03 -0300

    feat: DeleteCategoryById model created

--- source/model/Categories/index.js
+++ source/model/Categories/index.js
@@ -1,11 +1,13 @@
 const AddCategory = require('./AddCategory');
 const GetCategoryByName = require('./GetCategoryByName');
 const GetAllCategories = require('./GetAllCategories');
+const DeleteCategoryById = require('./DeleteCategoryById');
 
 const categoriesModel = {
   AddCategory,
   GetCategoryByName,
   GetAllCategories,
+  DeleteCategoryById,
 };
 
 module.exports = categoriesModel;
