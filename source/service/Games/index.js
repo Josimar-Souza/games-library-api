5c18a87ddace6647f7cf72790a82de46bab488d9
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-15 11:20:41 -0300

    feat: Added GetAllGames service added

--- source/service/Games/index.js
+++ source/service/Games/index.js
@@ -1,11 +1,13 @@
 const AddGame = require('./AddGame');
 const UpdateGameById = require('./UpdateGameById');
 const GetGameById = require('./GetGameById');
+const GetAllGames = require('./GetAllGames');
 
 const gamesService = {
   AddGame,
   UpdateGameById,
   GetGameById,
+  GetAllGames,
 };
 
 module.exports = gamesService;
